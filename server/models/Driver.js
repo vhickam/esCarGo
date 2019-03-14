4991bb31b48fa82a13402b2971ad81da297ea06b
Victoria Hickam <vhickam@gmail.com>
2019-03-14 18:54:27 -0400

    added username and password to models

--- server/models/Driver.js
+++ server/models/Driver.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const driverSchema = new Schema({
+  username: String,
+  password: String,
   firstname: String,
   lastname: String,
   license: String,
--- server/models/Shipper.js
+++ server/models/Shipper.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const shipperSchema = new Schema({
+  username: String,
+  password: String,
   firstname: String,
   lastname: String,
   packages: Array
