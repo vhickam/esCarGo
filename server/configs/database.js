7ade70a475790365caa75c044466877502b17b10
Victoria Hickam <vhickam@gmail.com>
2019-03-20 15:14:20 -0400

    test heroku

--- server/configs/database.js
+++ server/configs/database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 // Don't forget to set "MONGODB_URI" in ~/server/.env
-const uri = process.env.MONGODB_URI || `mongodb://localhost/please-set-process-env-mongodb-uri`;
-
+const uri = process.env.MONGODB_URI || `mongodb+srv://fitpal:fitpal@fitpal-ebrlo.mongodb.net/escargo?retryWrites=true`;
+console.log(uri)
 mongoose
   .connect(uri, { useNewUrlParser: true })
   .then(x => {
